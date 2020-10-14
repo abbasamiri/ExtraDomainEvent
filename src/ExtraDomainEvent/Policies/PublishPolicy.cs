85e8663be8b31f08195cbdb6165a7a8f0ebd9ba0
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-14 20:29:58 +0330

    Add PublishPolicy enum

---
+++ src/ExtraDomainEvent/Policies/PublishPolicy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtraDomainEvent.Policies
+{
+    [Flags]
+    public enum PublishPolicy
+    {
+        /// <summary>
+        /// No policy should be applied.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Prevent duplicate type.
+        /// </summary>
+        PreventDuplicateType,
+        /// <summary>
+        /// Prevent duplicate reference.
+        /// </summary>
+        PreventDuplicateReference,
+    }
+}
