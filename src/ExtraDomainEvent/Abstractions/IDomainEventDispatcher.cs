f268684cdaa74c59a22a6596161e4a680f800c90
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-14 20:28:53 +0330

    Add IDomainEventDispatcher interface

---
+++ src/ExtraDomainEvent/Abstractions/IDomainEventDispatcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using ExtraDomainEvent.Policies;
+
+namespace ExtraDomainEvent.Abstractions
+{
+    /// <summary>
+    /// Defines a dispatcher for the domain-event(s).
+    /// </summary>
+    public interface IDomainEventDispatcher : IDisposable
+    {
+        /// <summary>
+        /// Dispatches all domain-events.
+        /// </summary>
+        /// <param name="context">The instance of domain-event-context.</param>
+        /// <param name="dispatchPolicy">The dispatch policy.</param>
+        /// <typeparam name="TDomainEventContext">The type of domain-event-context.</typeparam>
+        /// <returns>A task representing invoking all handlers.</returns>
+        Task<DispatchResult[]> Dispatch<TDomainEventContext>(TDomainEventContext context, DispatchPolicy dispatchPolicy);
+    }
+}
