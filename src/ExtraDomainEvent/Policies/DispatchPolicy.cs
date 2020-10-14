f77e51a1087016e90662490469e30600b782f5d7
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-14 20:27:00 +0330

    Add DispatchPolicy enum

---
+++ src/ExtraDomainEvent/Policies/DispatchPolicy.cs
@@ -0,0 +1,18 @@
+﻿namespace ExtraDomainEvent.Policies
+{
+    /// <summary>
+    /// Represents the policy to dispatch multiple events.
+    /// </summary>
+    public enum DispatchPolicy
+    {
+        /// <summary>
+        /// Ignores Failure and continue.
+        /// </summary>
+        IgnoreFailureAndContinue,
+        
+        /// <summary>
+        /// First Failure terminates dispatching process.  
+        /// </summary>
+        TerminateOnFirstFailure,
+    }
+}
