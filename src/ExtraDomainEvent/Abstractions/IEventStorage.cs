bbc12e703e4b91e4ee08da44e67cf866c42866c5
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-14 20:29:34 +0330

    Add IEventStorage interface

---
+++ src/ExtraDomainEvent/Abstractions/IEventStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtraDomainEvent.Abstractions
+{
+    /// <summary>
+    /// Defines a <strong>List</strong> to store published domain-events.   
+    /// </summary>
+    public interface IEventStorage : IList<IDomainEvent>, IDisposable
+    {
+    }
+}
