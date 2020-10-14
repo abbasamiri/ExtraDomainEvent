d8dd810f07fef8b62ccfeb7a947ecceccfac9fb7
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-14 20:28:33 +0330

    Add IDomainEventContext interface

---
+++ src/ExtraDomainEvent/Abstractions/IDomainEventContext.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtraDomainEvent.Abstractions
+{
+    /// <summary>
+    /// Marker interface to represent a domain-event-context.
+    /// </summary>
+    public interface IDomainEventContext
+    {
+        
+    }
+}
