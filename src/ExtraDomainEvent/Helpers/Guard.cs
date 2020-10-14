c8ed8a2040c4b64b87735c1c04eb34bcc0312288
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-14 20:33:29 +0330

    Add Guard class

---
+++ src/ExtraDomainEvent/Helpers/Guard.cs
@@ -0,0 +1,39 @@
+﻿// Based on https://github.com/gaochundong/PracticeUnity/blob/master/Unity/Unity/Src/Utility/Guard.cs
+
+using System;
+
+namespace ExtraDomainEvent.Helpers
+{
+    /// <summary>
+    /// A static helper class that includes various parameter checking routines.
+    /// </summary>
+    public static class Guard
+    {
+        /// <summary>
+        /// Throws <see cref="ArgumentNullException"/> if the given argument is null.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"> if tested value if null.</exception>
+        /// <param name="argumentValue">Argument value to test.</param>
+        /// <param name="argumentName">Name of the argument being tested.</param>
+        public static void ArgumentNotNull(object argumentValue,
+            string argumentName)
+        {
+            if (argumentValue == null) throw new ArgumentNullException(argumentName);
+        }
+
+        /// <summary>
+        /// Throws an exception if the tested string argument is null or the empty string.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown if string value is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if the string is empty</exception>
+        /// <param name="argumentValue">Argument value to check.</param>
+        /// <param name="argumentName">Name of argument being checked.</param>
+        public static void ArgumentNotNullOrEmpty(string argumentValue,
+            string argumentName)
+        {
+            if (argumentValue == null) throw new ArgumentNullException(argumentName);
+            if (argumentValue.Length == 0)
+                throw new ArgumentException("The provided string argument must not be empty.", argumentName);
+        }
+    }
+}
