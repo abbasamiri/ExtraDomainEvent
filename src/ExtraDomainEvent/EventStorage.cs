1fcbe367b1b0c20c62f444b2643c02782ab353e2
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-14 20:41:16 +0330

    Add EventStorage class

---
+++ src/ExtraDomainEvent/EventStorage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using ExtraDomainEvent.Abstractions;
+
+namespace ExtraDomainEvent
+{
+    /// <summary>
+    /// The default implementation of <see cref="IEventStorage"/>
+    /// </summary>
+    public class EventStorage : List<IDomainEvent>, IEventStorage
+    {
+        public EventStorage()
+        {
+            
+        }
+        
+        public void Dispose()
+        {
+            Clear();
+        }
+    }
+}
