fc405f22f5d55354c84c097e1febd371096790df
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-16 09:27:06 +0330

    Add DomainEventOne class

---
+++ tests/ExtraDomainEvent.Tests.System/DomainEventOne.cs
@@ -0,0 +1,12 @@
+﻿using ExtraDomainEvent.Abstractions;
+
+namespace ExtraDomainEvent.Tests.System
+{
+    public class DomainEventOne : IDomainEvent
+    {
+        public DomainEventOne()
+        {
+            
+        }
+    }
+}
