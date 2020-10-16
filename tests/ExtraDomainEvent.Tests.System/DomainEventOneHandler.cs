9423c53b70053fa21f968f66fbfd172d12ff264d
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-16 09:27:36 +0330

    Add DomainEventOneHandler class

---
+++ tests/ExtraDomainEvent.Tests.System/DomainEventOneHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using ExtraDomainEvent.Abstractions;
+
+namespace ExtraDomainEvent.Tests.System
+{
+    public class DomainEventOneHandler : IDomainEventHandler<DomainEventOne,DomainEventContext>
+    {
+        public Task<DispatchResult> Handle(DomainEventOne domainEvent, DomainEventContext domainEventContext)
+        {
+            throw new global::System.NotImplementedException();
+        }
+    }
+}
