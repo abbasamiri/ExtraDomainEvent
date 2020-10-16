e036bbc38f440d929c4f0566c4ba1177487def51
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-16 07:44:18 +0330

    Add SecondDomainEventHandler class

---
+++ tests/ExtraDomainEvent.Tests/System/SecondDomainEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using ExtraDomainEvent.Abstractions;
+using ExtraDomainEvent.Exceptions;
+
+namespace ExtraDomainEvent.Tests.System
+{
+    public class SecondDomainEventHandler : IDomainEventHandler<SecondDomainEvent, MainDomainEventContext>
+    {
+        public Task<DispatchResult> Handle(SecondDomainEvent domainEvent, MainDomainEventContext domainEventContext)
+        {
+            return Task.FromResult(DispatchResult.Fail(new DomainEventException(domainEvent)));
+        }
+    }
+}
