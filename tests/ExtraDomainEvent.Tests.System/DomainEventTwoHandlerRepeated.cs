34ca2d1b085bf5b9731881327f56c9c7557af2ee
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-16 09:28:15 +0330

    Add DomainEventTwoHandlerRepeated class

---
+++ tests/ExtraDomainEvent.Tests.System/DomainEventTwoHandlerRepeated.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using ExtraDomainEvent.Abstractions;
+
+namespace ExtraDomainEvent.Tests.System
+{
+    public class DomainEventTwoHandlerRepeated : IDomainEventHandler<DomainEventTwo, DomainEventContext>
+    {
+        public Task<DispatchResult> Handle(DomainEventTwo domainEvent, DomainEventContext domainEventContext)
+        {
+            throw new global::System.NotImplementedException();
+        }
+    }
+}
