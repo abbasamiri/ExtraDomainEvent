6223ce611dd840d9467a9b5cc1700546ba06b077
abbasamiri <abbas.amiri.developer@outlook.com>
2020-10-16 07:43:44 +0330

    Add FirstDomainEventHandler class

---
+++ tests/ExtraDomainEvent.Tests/System/FirstDomainEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using ExtraDomainEvent.Abstractions;
+
+namespace ExtraDomainEvent.Tests.System
+{
+    public class FirstDomainEventHandler : IDomainEventHandler<FirstDomainEvent, MainDomainEventContext>
+    {
+        public Task<DispatchResult> Handle(FirstDomainEvent domainEvent, MainDomainEventContext domainEventContext)
+        {
+            return Task.FromResult(DispatchResult.Ok());
+        }
+    }
+}
